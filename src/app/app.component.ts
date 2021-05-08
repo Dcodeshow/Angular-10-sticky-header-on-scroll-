c18921064622e08ec7391d4c8e1727856dbf135f
DCODE SHOW <dcodeshow@gmail.com>
2021-05-09 02:22:33 +0530

    Sticky Header

---
+++ src/app/app.component.ts
@@ -0,0 +1,34 @@
+import { Component, HostListener } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+  navbarfixed:boolean = false;
+
+  @HostListener('window:scroll',['$event']) onscroll(){
+    if(window.scrollY > 100)
+    {
+      this.navbarfixed = true;
+    }
+    else
+    {
+      this.navbarfixed = false;
+    }
+  }
+
+}
+
+//Summary
+//We have use.
+//scroll event
+//function
+//ng class property binding
+//Hostlistener the main part, if u dont understand plz study about hostlistener
+//if condition
+//window.scrollY
+
+
